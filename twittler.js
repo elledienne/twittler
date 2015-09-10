9c32a0987cd2b1c38702cfc94e6fc6c2c99acb43
Lorenzo De Nobili <elledienne@gmail.com>
2015-09-10 16:45:18 +0200

    Now tweets timestamps are displayed. Added semantic markup

--- twittler.js
+++ twittler.js
@@ -13,14 +13,22 @@ $(document).ready(function(){
     }
     while(latestTweetIndex <= streams.home.length-1){
       var tweet = streams.home[latestTweetIndex];
-      var $tweet = $('<div></div>');
-      $tweet.text('@' + tweet.user + ': ' + tweet.message);
+      var $tweet = $('<article></article>');
+      $tweet.html(
+        '<section class="tweet-content">' + tweet.message + '</section>' +
+        '<section class="tweet-info">' +
+        '  <ul>' +
+        '    <li class="user">' + tweet.user + '</li>' +
+        '    <li class="timestamp"> ' + tweet.created_at + ' </li>' +
+        '  </ul>' +
+        '</section>'
+      );
       $tweet.prependTo($body);
       latestTweetIndex++;
     }
   };
 
-  // This function keep checking if new tweets are added to the streams.home array
+  // This function keeps checking if new tweets are added to the streams.home array
   var checkUpdates = function(){
     if(latestTweetIndex === null || latestTweetIndex !== streams.home.length){
       showTweet(); // If new tweets -> call the function to show them
